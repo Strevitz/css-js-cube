b4f610cb347078f8886f68fc3870edc4e17bf09f
Arthur <Strevitz@github.com>
2020-08-21 23:36:51 +0200

    commit

--- app.js
+++
@@ -1,9 +0,0 @@
-/*document.getElementByClass("front").onclick = function () {
-  this.classList.toggle("show-top");
-}; */
-
-function classToggle() {
-  this.classList.toggle("front");
-  this.classList.toggle("show-front");
-}
-document.querySelector(".front").addEventListener("click", classToggle);
